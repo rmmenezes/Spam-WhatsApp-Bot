f1d8ee245ce2c82297ec34141f3c1de02c17ba86
rmmenezes <ra29fa@gmail.com>
2018-11-02 23:55:34 -0300

    v01

--- main.js
+++ main.js
@@ -1,50 +1,23 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const {app, BrowserWindow} = require('electron');
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('ready', function(){
+  var mensagem = 'Amo Voce <3';
+  var numero =  '5518996562399';
+  mainWindow = new BrowserWindow()
+  mainWindow.loadURL('https://web.whatsapp.com/send?phone=' + numero + '&text=' + mensagem)
+  mainWindow.webContents.executeJavaScript(`
+    var enviado = false;
+    function tempo(){
+        var btsend = document.getElementsByClassName("_35EW6")[0];
+        var inputSend = document.getElementsByClassName("_2S1VP")[0];
+        if(typeof inputSend !== 'undefined' && inputSend.textContent && enviado == false){
+            btsend.click();
+            enviado = true;
+        }else if(enviado){
+            /* cancela */
+        }
+    }
+    setInterval(tempo,3000)
+  `)
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
